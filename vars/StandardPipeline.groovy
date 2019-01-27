2ea528da81b945abcf385204a4f51c1af6b773cc
John Fletcher K <john.kommuguri@thomsonreuters.com>
2019-01-28 00:42:02 +0530

    added sample vars file

---
+++ vars/StandardPipeline.groovy
@@ -0,0 +1,40 @@
+#!/usr/bin/env groovy
+
+def call(body){
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.deligate = config
+  body()
+
+  node{
+    //clean workspace befoe doing anything
+    deleteDir()
+
+    try{
+      stage('clone'){
+        checkout scm
+      }
+      stage('Build'){
+        sh "echo 'building ${config.projectName} ...'"
+      }
+      stage('Tests'){
+        parall 'static': {
+          sh "echo 'shell scripts to run static tests ...'"
+        },
+        'unit': {
+          sh "echo 'shell scripts to run unit tests ...'"
+        },
+        'integration': {
+          sh "echo 'shell scripts to run integration tests ...'"
+        }
+      }
+      stage('Deploy'){
+        sh "echo 'deploying to server ${config.serverDomain}...'"
+      }
+    } catch(err){
+      currentBuild.result = 'FAILED'
+      throw err
+    }
+
+  }
+}
